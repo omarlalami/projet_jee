d1ad45ee078b8bae5832a06a2ba93f04e0bfd9a6
l16024220 <l16024220@C-ID-S02-23.salsa.univ-amu.fr>
2019-11-22 16:31:45 +0100

    ajout config final

---
+++ gestioncommande-CoreDB/src/miage/gestioncommande/coreDB/ProduitM.java
@@ -0,0 +1,67 @@
+package miage.gestioncommande.coreDB;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import miage.gestioncommande.api.Prix;
+import miage.gestioncommande.api.Produit;
+
+@XmlRootElement
+public class ProduitM implements Produit {
+
+	private Long id;
+	
+	private String designation;
+	
+	@XmlElement(type=PrixM.class)
+	private List<Prix> prix;
+
+	/* (non-Javadoc)
+	 * @see miage.gestioncommande.coreM.Produit#getId()
+	 */
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	/* (non-Javadoc)
+	 * @see miage.gestioncommande.coreM.Produit#setId(java.lang.Long)
+	 */
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/* (non-Javadoc)
+	 * @see miage.gestioncommande.coreM.Produit#getDesignation()
+	 */
+	@Override
+	public String getDesignation() {
+		return designation;
+	}
+
+	/* (non-Javadoc)
+	 * @see miage.gestioncommande.coreM.Produit#setDesignation(java.lang.String)
+	 */
+	@Override
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	@Override
+	public List<Prix> getPrix() {
+		if(prix == null) prix = new ArrayList<>();
+		return prix;
+	}
+
+	@Override
+	public void setPrix(List<Prix> prix) {
+		this.prix = prix;
+		
+	}
+	
+}
